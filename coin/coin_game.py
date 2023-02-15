adc760b790e47512dbe2d6dd8391d34057e36bee
Wilbert213 <113620401+Wilbert213@users.noreply.github.com>
2023-02-15 12:30:41 -0500

    Add files via upload

--- coin/coin_game.py
+++ coin/coin_game.py
@@ -8,3 +8,25 @@ Skull= Actor("skull")
 Skull.pos = 100, 100
 
 Coin = Actor("coin")
+coin.pos= 200 , 200
+
+def draw():
+screen.fill("green")
+fox.draw()
+coin.draw()
+screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
+
+def place_coin():
+pass
+def time_up():
+pass
+def update():
+pass
+
+coin.x = randint(20, (WIDTH - 20))
+coin.y = randint(20, (HEIGHT - 20))
+def update():
+pass
+
+
+
